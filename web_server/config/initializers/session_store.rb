96815e1b20ef982fbd7c2464589c7439dafe1d0a
nguyenquangminh <nevermore164@gmail.com>
2018-04-25 03:17:46 +0700

    update

--- web_server/config/initializers/session_store.rb
+++ web_server/config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # Rails.application.config.session_store :redis_store, servers: ["redis://localhost:6379/1/session"]
-Rails.application.config.session_store :redis_store
+Rails.application.config.session_store :cookie_store, key: '_my_key'
