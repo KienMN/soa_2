b1aed82d272d9c31c039b191adbaaff4888c56ba
nguyenquangminh <nevermore164@gmail.com>
2018-05-01 23:28:16 +0700

    update

--- web_server/app/controllers/api/v1/student/comments_controller.rb
+++ web_server/app/controllers/api/v1/student/comments_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::Student::CommentsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action -> { pre_processing }
 
-  include Stduent::Comment::CommentHelper
+  include Student::Comment::CommentHelper
 
   def create
     render json: @action.status
